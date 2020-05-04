dada201b0f1a4b0648478e6edee449ff5eb62887
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-04 23:31:28 +0530

    [Vinita]ADDED:ResettingTheBoard

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome to Tic tac toe Game"
 
+declare -a board
+NUMBER_OF_ROWS=3
+NUMBER_OF_COLUMNS=3
+
+function initializingBoard(){
+
+	board=(0,0,0,0,0,0,0,0,0)
+
+}
+
+initializingBoard
